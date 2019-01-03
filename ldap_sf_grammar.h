4df1daf9db62c21a2b59e7d067f31d96edf5eeaf
David Bailey <dave.bailey@outlook.com>
2019-01-03 20:56:42 +0000

    Start cleanup of namespaces

--- ldap_sf_grammar.h
+++ ldap_sf_grammar.h
@@ -21,134 +21,139 @@ namespace  ldap { namespace sf
 using namespace boost::spirit;
 using namespace boost::spirit::qi;
 using namespace boost::spirit::unicode;
-using namespace boost::phoenix;
-using boost::spirit::qi::rule;
-using boost::spirit::unicode::space;
-using boost::spirit::unicode::space_type;
-using boost::spirit::unicode::alpha;
-using boost::spirit::unicode::alnum;
-using boost::spirit::unicode::char_;
-using boost::spirit::_1;
-using boost::spirit::_2;
-using boost::spirit::_3;
 
 struct Compiler
 {
-  template <typename A,
-            typename B = ::boost::spirit::unused_type,
-            typename C = ::boost::spirit::unused_type,
-            typename D = ::boost::spirit::unused_type>
-  struct result { using type = void; };
+    template <typename A,
+              typename B = ::boost::spirit::unused_type,
+              typename C = ::boost::spirit::unused_type,
+              typename D = ::boost::spirit::unused_type>
+        struct result { using type = void; };
 
-  void  operator()( NodeList &  nodes, Node &  node ) const;
+    void operator()(NodeList & nodes, Node &  node) const;
 
-  void  operator()( Node &  node, NodeList &  nodes, FilterComp  comp ) const;
+    void operator()(Node & node, NodeList & nodes, FilterComp comp) const;
 
-  void  operator()( Node &  node, Node &  nodeNest ) const;
+    void operator()(Node & node, Node & nodeNest) const;
 
-  void  operator()( ItemPtr &  item, AttrOptionsList &  attrs,
-                    SimpleItemOp  op, Value &  value ) const;
+    void operator()(ItemPtr & item, AttrOptionsList & attrs,
+                    SimpleItemOp  op, Value & value) const;
 
-  void  operator()( AttrOptionsList &  attrs, Attr &  value,
-                    boost::optional< AttrOptionsList > &  attrs_options ) const;
+    void operator()(AttrOptionsList & attrs, Attr & value,
+                    boost::optional<AttrOptionsList> & attrs_options) const;
 
-  void  operator()( ItemPtr &  item, AttrOptionsList &  attrs,
-                    ValueListMore &  values ) const;
+    void operator()(ItemPtr &  item, AttrOptionsList & attrs,
+                    ValueListMore & values) const;
 
-  void  operator()( ValueListMore &  values,
-                    boost::optional< Value > &  value_first,
-                    ValueList &  values_middle,
-                    boost::optional< Value > &  value_last ) const;
+    void operator()(ValueListMore & values,
+                    boost::optional<Value> & value_first,
+                    ValueList & values_middle,
+                    boost::optional<Value> & value_last) const;
 };
 
-template<typename  Iterator>
-struct Grammar : grammar<Iterator, Node(), space_type>
+template<typename Iterator>
+struct Grammar : grammar<Iterator,
+                         Node(),
+                         boost::spirit::unicode::space_type>
 {
-  Grammar();
+    using NodeRule = ::boost::spirit::qi::rule<Iterator,
+                                               Node(),
+                                               boost::spirit::unicode::space_type>;
+    using NodeListRule = ::boost::spirit::qi::rule<Iterator,
+                                                   NodeList(),
+                                                   boost::spirit::unicode::space_type>;
+    using ItemPtrRule = ::boost::spirit::qi::rule<Iterator,
+                                                  ItemPtr(),
+                                                  boost::spirit::unicode::space_type>;
+    Grammar();
 
-  rule< Iterator, Node(), space_type >             filter;
+    NodeRule filter;
+    NodeListRule filterlist;
+    NodeRule filtercomp;
+    NodeRule and_expr;
+    NodeRule or_expr;
+    NodeRule not_expr;
+    ItemPtrRule item;
+    ItemPtrRule simple;
+    ItemPtrRule substring;
+    ItemPtrRule extensible;
 
-  rule< Iterator, NodeList(), space_type >         filterlist;
+    ::boost::spirit::qi::rule<Iterator, SimpleItemOp(), ::boost::spirit::unicode::space_type>
+        filtertype;
 
-  rule< Iterator, Node(), space_type >             filtercomp;
+    ::boost::spirit::qi::rule<Iterator, AttrOptionsList(), ::boost::spirit::unicode::space_type>
+        attr;
 
-  rule< Iterator, Node(), space_type >             and_expr;
+    ::boost::spirit::qi::rule<Iterator, Attr(), ::boost::spirit::unicode::space_type>
+        attr_item;
 
-  rule< Iterator, Node(), space_type >             or_expr;
+    ::boost::spirit::qi::rule<Iterator, Value(), ::boost::spirit::unicode::space_type>
+        value;
 
-  rule< Iterator, Node(), space_type >             not_expr;
+    ::boost::spirit::qi::rule<Iterator, ValueListMore(), ::boost::spirit::unicode::space_type>
+        values;
 
-  rule< Iterator, ItemPtr(), space_type >          item;
+    ::boost::spirit::qi::rule<Iterator, MatchingRule(), ::boost::spirit::unicode::space_type>
+        matchingrule;
 
-  rule< Iterator, ItemPtr(), space_type >          simple;
+    ::boost::spirit::qi::uint_parser<unsigned char, 16, 2, 2> hex_2;
 
-  rule< Iterator, ItemPtr(), space_type >          substring;
+    ::boost::spirit::qi::uint_parser<unsigned int, 16, 8, 8>  hex_8;
 
-  rule< Iterator, ItemPtr(), space_type >          extensible;
-
-  rule< Iterator, SimpleItemOp(), space_type >     filtertype;
-
-  rule< Iterator, AttrOptionsList(), space_type >  attr;
-
-  rule< Iterator, Attr(), space_type >             attr_item;
-
-  rule< Iterator, Value(), space_type >            value;
-
-  rule< Iterator, ValueListMore(), space_type >    values;
-
-  rule< Iterator, MatchingRule(), space_type >     matchingrule;
-
-  uint_parser< unsigned char, 16, 2, 2 >           hex_2;
-
-  uint_parser< unsigned int, 16, 8, 8 >            hex_8;
-
-  function< Compiler >                             op;
+    ::boost::phoenix::function<Compiler> op;
 };
 
-
-template  < typename  Iterator >
-Grammar< Iterator >::Grammar() : Grammar::base_type( filter )
+template<typename Iterator>
+Grammar<Iterator>::Grammar() : Grammar::base_type(filter)
 {
-  filter = lit( '(' ) >> filtercomp >> lit( ')' );
+    using ::boost::spirit::unicode::space;
+    using ::boost::spirit::unicode::alpha;
+    using ::boost::spirit::unicode::alnum;
+    using ::boost::spirit::unicode::char_;
+    using ::boost::spirit::_1;
+    using ::boost::spirit::_2;
+    using ::boost::spirit::_3;
 
-  filtercomp = and_expr | or_expr | not_expr | item;
+    filter = lit('(') >> filtercomp >> lit(')');
 
-  filterlist = +( filter[ op( _val, _1 ) ] );
+    filtercomp = and_expr | or_expr | not_expr | item;
 
-  and_expr = lit( '&' ) >> filterlist[ op( _val, _1, FC_And ) ];
+    filterlist = +(filter[op(_val, _1)]);
 
-  or_expr = lit( '|' ) >> filterlist[ op( _val, _1, FC_Or ) ];
+    and_expr = lit('&') >> filterlist[op(_val, _1, FC_And)];
 
-  not_expr = lit( '!' ) >> filter[ op( _val, _1 ) ];
+    or_expr = lit('|') >> filterlist[op(_val, _1, FC_Or)];
 
-  item = substring | simple | extensible;
+    not_expr = lit('!') >> filter[op(_val, _1)];
 
-  simple = ( attr >> filtertype >> value )[ op( _val, _1, _2, _3 ) ];
+    item = substring | simple | extensible;
 
-  attr = ( attr_item >> -( lit( ';' ) >> attr ) )[ op( _val, _1, _2 ) ];
+    simple = (attr >> filtertype >> value)[op(_val, _1, _2, _3)];
 
-  attr_item = alpha >> *( alnum | '-' );
+    attr = (attr_item >> -(lit(';') >> attr))[op(_val, _1, _2)];
 
-  value = +( lit( '\\' ) >> ( hex_8 | hex_2 ) | ( char_ - '*' - ')' ) );
+    attr_item = alpha >> *(alnum | '-');
 
-  filtertype = lit( '=' )[ _val = SIO_Equal ] |
-      lit( "~=" )[ _val = SIO_Approx ] |
-      lit( ">=" )[ _val = SIO_Greater ] |
-      lit( "<=" )[ _val = SIO_Less ];
+    value = +(lit('\\') >> (hex_8 | hex_2) | (char_ - '*' - ')'));
 
-  substring = ( attr >> lit( '=' ) >> values )[ op( _val, _1, _2 ) ];
+    filtertype = lit('=')[_val = SIO_Equal] |
+        lit("~=")[_val = SIO_Approx] |
+        lit(">=")[_val = SIO_Greater] |
+        lit("<=")[_val = SIO_Less];
 
-  values = ( -( value ) >> lit( '*' ) >> *( value >> lit( '*' ) ) >>
-             -( value ) )[ op( _val, _1, _2, _3 ) ];
+    substring = (attr >> lit('=') >> values)[op(_val, _1, _2)];
 
-  matchingrule = alnum >> *( alnum | '-' | '.' );
+    values = (-(value) >> lit('*') >> *(value >> lit('*')) >>
+        -(value))[op(_val, _1, _2, _3)];
 
-  extensible = ( ( attr >> -( lit ( ":dn" ) ) >>
-                   -( lit( ':' ) >> matchingrule ) >> lit( ":=" ) >> value )
-                 |
-                 ( -( lit( ":dn" ) ) >> lit( ':' ) >> matchingrule >>
-                   lit( ":=" ) >> value ) )
-      [ _pass = false /* extensible items are not implemented! */ ];
+    matchingrule = alnum >> *(alnum | '-' | '.');
+
+    extensible = ((attr >> -(lit(":dn")) >>
+        -(lit(':') >> matchingrule) >> lit(":=") >> value)
+        |
+        (-(lit(":dn")) >> lit(':') >> matchingrule >>
+            lit(":=") >> value))
+        [_pass = false /* extensible items are not implemented! */];
 }
 
 }   // namespace sf
